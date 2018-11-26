e07478fbbde800ae4d6c57a30693a7875b1c48dd
muerdis <a.shulaev@itdept.cloud>
2018-11-26 16:02:41 +0300

    (Frontend) Тестовый CSRF, пока хардкод.

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const http = axios.create();
 
 const api = axios.create({
+  headers: {
+    "X-CSRFToken": 'ZajyhyQJZdsC5zBfAyCenEfqzQypoB71UDyBQTMyf63uwSG20aK8wU1RAbRZy2mH',
+  },
   baseURL: 'http://localhost:8000/api/',
 });
 

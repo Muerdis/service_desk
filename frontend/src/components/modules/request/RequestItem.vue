fb0b6bdda33efacdf80d2c5c77978e876d622367
muerdis <a.shulaev@itdept.cloud>
2018-11-26 16:04:10 +0300

    (Frontend) Изменение статусов заявок и их удаление.

--- frontend/src/components/modules/request/RequestItem.vue
+++ frontend/src/components/modules/request/RequestItem.vue
@@ -9,18 +9,18 @@
         p.faded-text {{ request.created_user_name }}
         p.faded-text {{ request.date_created }}
       v-card-actions(v-if="mode === 'incoming'")
-        v-btn(@click="" color="success")
+        v-btn(@click="changeStatus(2)" color="success")
           v-flex
             span Взять в работу
-        v-btn(@click="" color="error")
+        v-btn(@click="changeStatus(3)" color="error")
           v-flex
             span Отклонить
       v-card-actions(v-if="mode === 'outgoing'")
-        v-btn(@click="" color="error")
+        v-btn(@click="deleteRequest" color="error")
           v-flex
             span Удалить
       v-card-actions(v-if="mode === 'in-work'")
-        v-btn(@click="" color="success")
+        v-btn(@click="changeStatus(3)" color="success")
           v-flex
             span Завершить
 </template>
@@ -36,6 +36,19 @@ export default {
       type: String,
     },
   },
+  methods: {
+    changeStatus(status) {
+      const info = {
+        id: this.request.id,
+        status,
+      };
+
+      this.$store.dispatch('request/changeStatus', info);
+    },
+    deleteRequest() {
+      this.$store.dispatch('request/deleteRequest', this.request.id);
+    }
+  }
 };
 </script>
 
--- frontend/src/store/modules/request/actions.js
+++ frontend/src/store/modules/request/actions.js
@@ -8,6 +8,27 @@ async function getRequests(context) {
   context.commit('REQUESTS', infoResponse.data.results);
 }
 
+async function changeStatus(context, info) {
+  const patchUrl = `/requests/${info.id}/`;
+  const data = {
+    status: info.status
+  };
+  const patchPromise = requests.api.patch(patchUrl, data);
+  await Promise.resolve(patchPromise);
+
+  getRequests(context)
+}
+
+async function deleteRequest(context, id) {
+  const deleteUrl = `/requests/${id}/`;
+  const deletePromise = requests.api.delete(deleteUrl);
+  await Promise.resolve(deletePromise);
+
+  getRequests(context)
+}
+
 export default {
   getRequests,
+  changeStatus,
+  deleteRequest,
 };

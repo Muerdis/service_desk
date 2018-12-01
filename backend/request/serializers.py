e81fe6224dd42733babb4b848099cb006145b649
Muerdis <bahamat95@gmail.com>
2018-12-01 15:08:14 +0300

    (Backend) добавлено ФИО назначенного лица в API заявок.

--- backend/request/serializers.py
+++ backend/request/serializers.py
@@ -11,6 +11,7 @@ class RequestReasonSerializer(serializers.ModelSerializer):
 
 class RequestSerializer(serializers.ModelSerializer):
     created_user_name = serializers.CharField(source='created_user.get_full_name', read_only=True)
+    assigned_user_name = serializers.CharField(source='assigned_user.get_full_name', read_only=True)
     reason_name = serializers.CharField(source='request_reason.text', read_only=True)
     reason_type_name = serializers.CharField(source='request_reason.request_type.name', read_only=True)
     date_created = serializers.DateTimeField(format='%d-%m-%Y %H:%M')
@@ -19,7 +20,7 @@ class RequestSerializer(serializers.ModelSerializer):
         model = Request
         fields = (
             'id', 'title', 'text', 'date_created', 'status', 'request_reason', 'reason_type_name',
-            'reason_name', 'assigned_user', 'created_user', 'created_user_name'
+            'reason_name', 'assigned_user', 'created_user', 'created_user_name', 'assigned_user_name'
         )
 
 

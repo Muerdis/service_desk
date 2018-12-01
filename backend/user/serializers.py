8fdc1050c6c3437201cbd123480ab1844638fe0b
Muerdis <bahamat95@gmail.com>
2018-12-01 17:03:50 +0300

    (Backend) В API пользователей добавлено поле для ФИО.

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -4,9 +4,11 @@ from user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
     class Meta:
         model = User
         fields = (
-            'id', 'username', 'first_name',
+            'id', 'username', 'full_name', 'first_name',
             'last_name', 'patronymic', 'department'
         )

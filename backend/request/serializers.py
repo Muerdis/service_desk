892f40753225afe216536f5b418d8b83e2b7d827
muerdis <a.shulaev@itdept.cloud>
2018-12-03 12:38:52 +0300

    (Backend) Вывод в API заявки названий шаблонов.

--- backend/request/serializers.py
+++ backend/request/serializers.py
@@ -15,12 +15,19 @@ class RequestSerializer(serializers.ModelSerializer):
     reason_name = serializers.CharField(source='request_reason.text', read_only=True)
     reason_type_name = serializers.CharField(source='request_reason.request_type.name', read_only=True)
     date_created = serializers.DateTimeField(format='%d-%m-%Y %H:%M', read_only=True)
+    device_templates_names = serializers.SerializerMethodField(read_only=True)
+
+    @staticmethod
+    def get_device_templates_names(instance):
+        names = instance.device_templates.values_list('name', flat=True)
+        return names
 
     class Meta:
         model = Request
         fields = (
             'id', 'title', 'text', 'date_created', 'status', 'request_reason', 'reason_type_name', 'reason_name',
-            'assigned_user', 'created_user', 'created_user_name', 'assigned_user_name', 'device_templates'
+            'assigned_user', 'created_user', 'created_user_name',
+            'assigned_user_name', 'device_templates', 'device_templates_names'
         )
 
 

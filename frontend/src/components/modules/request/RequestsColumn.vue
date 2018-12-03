27f6d955a8e0c42d188011af37d51061d3377c01
muerdis <a.shulaev@itdept.cloud>
2018-12-03 13:56:35 +0300

    (Frontend) Drag and drop в agile.

--- frontend/src/components/modules/request/RequestsColumn.vue
+++ frontend/src/components/modules/request/RequestsColumn.vue
@@ -5,24 +5,30 @@
         v-card(dark :color="color")
           v-card-title(primary-title)
             .headline(style="text-align: center; width: 100%;") {{ title }}
-      v-flex(xs12 v-for="request in requests" :key="request.id")
-        v-card
-          v-card-title(primary-title)
-            span.title(
-              style="text-align: center; width: 100%; margin-bottom: 10px;"
-            ) {{ request.title }}
-            p Тип: {{ request.reason_type_name }}
-            p Причина: {{ request.reason_name }}
-            p Назначена на: {{ request.assigned_user_name }}
-            hr
-            p {{ request.text.slice(0, 80) }}...
-            p.faded-text {{ request.created_user_name }}
-            p.faded-text {{ request.date_created }}
+      draggable(v-model="requests")
+        v-flex(xs12 v-for="request in requests" :key="request.id")
+          v-card
+            v-card-title(primary-title)
+              span.title(
+                style="text-align: center; width: 100%; margin-bottom: 10px;"
+              ) {{ request.title }}
+              p Тип: {{ request.reason_type_name }}
+              p Причина: {{ request.reason_name }}
+              p Назначена на: {{ request.assigned_user_name }}
+              hr
+              p {{ request.text.slice(0, 80) }}...
+              p.faded-text {{ request.created_user_name }}
+              p.faded-text {{ request.date_created }}
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+
 export default {
   name: 'RequestsColumn',
+  components: {
+    draggable,
+  },
   props: {
     color: {
       type: String,

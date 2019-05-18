52406400484c43d5421de02df51294b801303a40
muerdis <a.shulaev@itdept.cloud>
2019-05-18 06:45:15 +0300

    Доделаны модели модуля "Новости"

--- backend/news/models.py
+++ backend/news/models.py
@@ -29,38 +29,3 @@ class News(models.Model):
     class Meta:
         verbose_name = _('Новость')
         verbose_name_plural = _('Новости')
-
-
-class Comment(models.Model):
-    """Комментарий"""
-
-    text = models.CharField(
-        max_length=100,
-        verbose_name='Текст',
-    )
-    news = models.ForeignKey(
-        'news.News',
-        verbose_name='Новость',
-        on_delete=models.CASCADE
-    )
-    date_created = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата и время создания'
-    )
-    created_user = models.ForeignKey(
-        'user.User',
-        verbose_name='Создатель комментария',
-        on_delete=models.CASCADE,
-        related_name='news_comments'
-    )
-
-    def __str__(self):
-        return '{0} {1} {2}'.format(
-            self.news.title,
-            self.created_user.get_full_name(),
-            self.date_created
-        )
-
-    class Meta:
-        verbose_name = _('Комментарий')
-        verbose_name_plural = _('Комментарии')
